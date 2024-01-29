dc5395ea6991610d71c2154bfd66cdbd81894ace
chohon <siaksiakx@gmail.com>
2024-01-29 21:09:29 +0900

    Coupon 발급 관련 Template 구현

--- server/event/views.py
+++ server/event/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.template import loader
 
 from work.models import Work
+from coupon.models import Coupon
 
 def index(request):
     works = Work.objects.all()
+    coupon = Coupon.objects.get(pk=1)
 
     template = loader.get_template('event/index.html')
-    context = { 'works': works }
+    context = { 'works': works, 'couponCount' : coupon.count}
     return HttpResponse(template.render(context, request))
